23f62634361be2e4f22e1a6abd85447aa083d1b1
BurakKolay <burakkolay487@gmail.com>
2022-08-15 02:41:27 +0300

    Credit exception

---
+++ src/main/java/com/todeb/bkolay/creditapplication/exception/CreditApplicationException.java
@@ -0,0 +1,7 @@
+package com.todeb.bkolay.creditapplication.exception;
+
+public class CreditApplicationException extends RuntimeException{
+    public CreditApplicationException(String message){
+        super(message);
+    }
+}
